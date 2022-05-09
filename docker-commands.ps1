f243223752d40d59cbf393891d8c8affbd79aed4
Andrew Antaya <aantaya@email.arizona.edu>
2022-05-09 12:35:26 -0700

    add docker commands for launching container

---
+++ docker-commands.ps1
@@ -0,0 +1,5 @@
+# local r docker container running on port 80
+# this command disables authentication so only use it on a private network
+
+docker run -d -e DISABLE_AUTH=true --rm -p 127.0.0.3:80:8787 -v C:/Users/andre/Dropbox/Dev/Data7_EDA_In_R_Workshops:/home/rstudio/exploratory-analysis rocker/verse:4.2.0
+
